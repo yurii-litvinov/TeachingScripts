536a8554d6f0b9ce0a819cfec7c7f1ef3543b30b
yurii-litvinov <yurii.litvinov@gmail.com>
2021-01-12 19:26:08 +0300

    Added small script to parse information about themes and advisors of qualification works

--- AdcEditor/Program.fs
+++ AdcEditor/Program.fs
@@ -5,15 +5,16 @@ open AdcOperational
 open CurriculumUtils
 open WorkDistributionData
 open HardcodedData
+open VkrAdvisorsParser
 
 [<EntryPoint>]
 let main _ =
 
     // logIn "../../adcCredentials.txt"
 
-    doEverythingRight 
-        "20_5162_1.docx" 
-        "../../adcCredentials.txt"
+    //doEverythingRight 
+    //    "20_5162_1.docx" 
+    //    "../../adcCredentials.txt"
 
     //doMagic
     //    "20_5162_1.docx" 
@@ -27,6 +28,11 @@ let main _ =
     //    ["Демьянович Юрий Казимирович"]
     //    ["Лабораторные работы", 16; "Консультации", 2; "Промежуточная аттестация (экз)", 2; "Лекции", 48]
     //    ["405"; "2448"]
+
+    let students = parseVkrAdvisors "../../Приложение к приказу ВКР и НР5006 23.11.2020 20.05.01.docx"
+
+    students 
+    |> Seq.iter (fun s -> printfn "%s" s.advisor)
 
     printfn "%s" "Done!"
     0
---
+++ AdcEditor/VkrAdvisorsParser.fs
@@ -0,0 +1,61 @@
+﻿module VkrAdvisorsParser
+
+open DocumentFormat.OpenXml.Packaging
+open DocumentFormat.OpenXml.Wordprocessing
+
+type VkrRecord = 
+    {
+        studentName: string
+        theme: string
+        englishTheme: string
+        advisor: string
+        advisorJobTitle: string
+        chair: string
+    }
+
+let parseRow (row: TableRow) =
+    let value rowNum =
+        row.Elements<TableCell>() |> Seq.item rowNum |> fun x -> x.InnerText
+
+    let themeCell = row.Elements<TableCell>() |> Seq.item 2
+    let runs = themeCell.Descendants<Run>()
+
+    if runs |> Seq.length < 3 then
+        { 
+            studentName = value 1
+            theme = ""
+            englishTheme = ""
+            advisor = ""
+            advisorJobTitle = ""
+            chair = ""
+        }
+    else
+        let mutable theme = ""
+        let mutable englishTheme = ""
+        let mutable seenBreak = false
+        for run in runs do
+            for element in run.ChildElements do
+                match element with
+                | :? Text when not seenBreak -> theme <- theme + element.InnerText
+                | :? Text when seenBreak -> englishTheme <- englishTheme + element.InnerText
+                | :? Break -> seenBreak <- true
+                | _ -> ()
+
+        { 
+            studentName = value 1
+            theme = theme
+            englishTheme = englishTheme
+            advisor = value 3
+            advisorJobTitle = value 4
+            chair = value 5
+        }
+
+let parseVkrAdvisors (fileName: string) :VkrRecord seq =
+    use wordDocument = WordprocessingDocument.Open(fileName, false)
+    
+    let body = wordDocument.MainDocumentPart.Document.Body
+    let mainTable = body.Elements<Table>() |> Seq.skip 1 |> Seq.head
+
+    mainTable.Elements<TableRow>() 
+    |> Seq.skip 2
+    |> Seq.map parseRow
