fc6cf0594c6e0f91781da1b50c1fd7d74836eaf8
yurii-litvinov <yurii.litvinov@gmail.com>
2021-03-14 21:55:41 +0300

    Support for numberings and hyperlinks, finally allowing to properly format literature.

--- RpdPatcher/ProgramPatcher.fs
+++ RpdPatcher/ProgramPatcher.fs
@@ -6,12 +6,14 @@ open System.IO
 open DocumentFormat.OpenXml
 open ProgramContentChecker
 open CurriculumUtils
+open System
 
 type Justification =
     | Center
     | Stretch
+    | Left
 
-let createParagraph text (justification: Justification) (bold: bool) (italic: bool) =
+let createParagraph text (justification: Justification) (bold: bool) (italic: bool) (indent: bool) =
     let text = Text(text)
     let run = Run()
 
@@ -25,9 +27,9 @@ let createParagraph text (justification: Justification) (bold: bool) (italic: bo
     let size = FontSize(Val = StringValue("24"))
     let sizeCs = FontSizeComplexScript(Val = StringValue("24"))
     
-    runProperties.AppendChild(fonts) |> ignore
-    runProperties.AppendChild(size) |> ignore
-    runProperties.AppendChild(sizeCs) |> ignore
+    runProperties.AppendChild fonts |> ignore
+    runProperties.AppendChild size |> ignore
+    runProperties.AppendChild sizeCs |> ignore
 
     if bold then 
         runProperties.AppendChild(Bold()) |> ignore
@@ -38,10 +40,15 @@ let createParagraph text (justification: Justification) (bold: bool) (italic: bo
     run.AppendChild(text) |> ignore
 
     let paragraphProperties = ParagraphProperties()
-    let indent = Indentation(FirstLine = StringValue("720"))
-    let justificationValue = if justification = Center then JustificationValues.Center else JustificationValues.Both
-    if justification <> Justification.Center then
+    let justificationValue = 
+        match justification with
+        | Center -> JustificationValues.Center 
+        | Stretch -> JustificationValues.Both
+        | Left -> JustificationValues.Left
+
+    if justification <> Justification.Center && indent then
         // Do not indent paragraphs with centering.
+        let indent = Indentation(FirstLine = StringValue("720"))
         paragraphProperties.Indentation <- indent
     let justification = Justification(Val = EnumValue<_>(justificationValue))
     paragraphProperties.Justification <- justification
@@ -51,6 +58,118 @@ let createParagraph text (justification: Justification) (bold: bool) (italic: bo
     paragraph.AppendChild paragraphProperties |> ignore
     paragraph.AppendChild run |> ignore
     paragraph
+
+let createNumbering (document: WordprocessingDocument) =
+    let numberingDefinitionsPart = document.MainDocumentPart.NumberingDefinitionsPart
+    if numberingDefinitionsPart = null then
+        document.MainDocumentPart.AddNewPart<NumberingDefinitionsPart>() |> ignore
+        document.MainDocumentPart.NumberingDefinitionsPart.Numbering <- Numbering()
+    let numberingPart = document.MainDocumentPart.NumberingDefinitionsPart.Numbering
+    let lastAbstractNumberingId = 
+        if numberingPart.Descendants<AbstractNum>() |> Seq.isEmpty then
+            0
+        else
+            numberingPart.Descendants<AbstractNum>() 
+            |> Seq.map (fun an -> an.AbstractNumberId.Value) 
+            |> Seq.sortDescending 
+            |> Seq.head
+
+    let abstractNum = AbstractNum()
+    abstractNum.AbstractNumberId <- Int32Value(lastAbstractNumberingId + 1)
+    let lvl0 = Level()
+    lvl0.LevelIndex <- Int32Value 0
+    lvl0.StartNumberingValue <- StartNumberingValue(Val = Int32Value 1)
+    lvl0.NumberingFormat <- NumberingFormat(Val = EnumValue<NumberFormatValues>(NumberFormatValues.Decimal))
+    lvl0.LevelText <- LevelText(Val = StringValue "%1.")
+    lvl0.LevelJustification <- LevelJustification(Val = EnumValue<LevelJustificationValues>(LevelJustificationValues.Left))
+
+    let lvl0PPr = ParagraphProperties()
+    lvl0PPr.Indentation <- Indentation(Left = StringValue("360"), FirstLine = StringValue("0"), Hanging = StringValue("360"))
+    lvl0.AppendChild lvl0PPr |> ignore
+
+    let lvl0RunPr = RunProperties()
+    let fonts = 
+        RunFonts
+            (Ascii = StringValue("Times New Roman"), 
+             HighAnsi = StringValue("Times New Roman"), 
+             ComplexScript = StringValue("Times New Roman"))
+
+    let size = FontSize(Val = StringValue("24"))
+    let sizeCs = FontSizeComplexScript(Val = StringValue("24"))
+    
+    lvl0RunPr.AppendChild fonts |> ignore
+    lvl0RunPr.AppendChild size |> ignore
+    lvl0RunPr.AppendChild sizeCs |> ignore
+    lvl0.AppendChild lvl0RunPr |> ignore
+
+    abstractNum.AppendChild lvl0 |> ignore
+
+    numberingPart.AppendChild abstractNum |> ignore
+
+    let num = NumberingInstance()
+    let lastNumberingId = 
+        if numberingPart.Descendants<NumberingInstance>() |> Seq.isEmpty then
+            0
+        else
+            numberingPart.Descendants<NumberingInstance>() 
+            |> Seq.map (fun n -> n.NumberID.Value) 
+            |> Seq.sortDescending 
+            |> Seq.head
+
+    num.NumberID <- Int32Value(lastNumberingId + 1)
+
+    let abstractNumberingRef = AbstractNumId(Val = Int32Value(lastAbstractNumberingId + 1))
+    num.AppendChild abstractNumberingRef |> ignore
+
+    numberingPart.AppendChild num |> ignore
+
+    num.NumberID
+
+let createHyperlink link (document: WordprocessingDocument) =
+    let text = Text(link)
+    let run = Run()
+
+    let runProperties = RunProperties()
+    let fonts = 
+        RunFonts
+            (Ascii = StringValue("Times New Roman"), 
+             HighAnsi = StringValue("Times New Roman"), 
+             ComplexScript = StringValue("Times New Roman"))
+
+    let size = FontSize(Val = StringValue("24"))
+    let sizeCs = FontSizeComplexScript(Val = StringValue("24"))
+    let underline = Underline(Val = EnumValue<_>(UnderlineValues.Single))
+    let color = Color(ThemeColor = EnumValue<_>(ThemeColorValues.Hyperlink))
+
+    runProperties.AppendChild fonts |> ignore
+    runProperties.AppendChild size |> ignore
+    runProperties.AppendChild sizeCs |> ignore
+    runProperties.AppendChild underline|> ignore
+    runProperties.AppendChild color|> ignore
+
+    run.AppendChild(runProperties) |> ignore
+    run.AppendChild(text) |> ignore
+
+    let paragraphProperties = ParagraphProperties()
+
+    let indent = Indentation(Left = StringValue("360"))
+    paragraphProperties.Indentation <- indent
+    let justification = Justification(Val = EnumValue<_>(JustificationValues.Left))
+    paragraphProperties.Justification <- justification
+    paragraphProperties.SpacingBetweenLines <- SpacingBetweenLines(After = StringValue("120"))
+
+    let linkParagraph = Paragraph()
+    linkParagraph.AppendChild paragraphProperties |> ignore
+
+    let rel = document.MainDocumentPart.AddHyperlinkRelationship(Uri(link), true)
+
+    let hyperlink = Hyperlink()
+    hyperlink.AppendChild run |> ignore
+    hyperlink.Id <- StringValue(rel.Id)
+    hyperlink.History <- OnOffValue.FromBoolean(true)            
+
+    linkParagraph.AppendChild hyperlink |> ignore
+    linkParagraph
 
 let findParagraph (body: Body) (text: string) =
     let paragraphs = body.Descendants<Paragraph>()
@@ -102,7 +221,7 @@ let patchSection (body: Body) (sectionName: string) (replaceTo: string list) =
 
             target.Remove()
             for replacePart in replaceTo |> List.rev do
-                let paragraph = createParagraph replacePart Stretch false false
+                let paragraph = createParagraph replacePart Stretch false false true
                 sectionHeader.InsertAfterSelf paragraph |> ignore
     else
         printfn "Секция '%s' не найдена, замена не произведена!" sectionName
@@ -114,7 +233,7 @@ let addGovernment (body: Body) =
         let university = 
             body.Descendants<Paragraph>() 
             |> Seq.find (fun p -> p.InnerText.Contains("Санкт-Петербургский государственный университет"))
-        let government = createParagraph "Правительство Российской Федерации" Center true false
+        let government = createParagraph "Правительство Российской Федерации" Center true false true
         let props = government.Descendants<RunProperties>() |> Seq.exactlyOne
         let spacing = Spacing(Val = Int32Value(20))
         props.AppendChild spacing |> ignore
@@ -127,9 +246,9 @@ let addStPetersburg (errors: string list) (body: Body) =
             let nextSectionHeader = nextSectionHeader.Value
             removePreviousParagraphIfEmpty body nextSectionHeader false
             printfn "Добавляю 'Санкт-Петербург 2020' примерно в титульник"
-            let stPetersburg = createParagraph "Санкт-Петербург" Center false false
+            let stPetersburg = createParagraph "Санкт-Петербург" Center false false true
             nextSectionHeader.InsertBeforeSelf stPetersburg |> ignore
-            let year = createParagraph "2020" Center false false
+            let year = createParagraph "2020" Center false false true
             nextSectionHeader.InsertBeforeSelf year |> ignore
 
             let breakElement = Break(Type = EnumValue<BreakValues>(BreakValues.Page))
@@ -157,7 +276,7 @@ let addCompetencesToLearningOutcomes (content: ProgramContent) (body: Body) =
         let nextSectionHeader = findParagraph body "Перечень и объём активных и интерактивных форм учебных занятий"
         if nextSectionHeader.IsSome then
             printfn "Вставляю стандартную фразу про компетенции в раздел 1.3."
-            let p = createParagraph "Дисциплина участвует в формировании компетенций обучающихся по образовательной программе, установленных учебным планом для данной дисциплины." Stretch false false
+            let p = createParagraph "Дисциплина участвует в формировании компетенций обучающихся по образовательной программе, установленных учебным планом для данной дисциплины." Stretch false false true
             removePreviousParagraphIfEmpty body nextSectionHeader.Value true
             nextSectionHeader.Value.InsertBeforeSelf p |> ignore
         else
@@ -167,18 +286,18 @@ let addCompetencesToAttestationMaterials (content: ProgramContent) (curriculum: 
     if content |> controlMaterialsShallReferenceCompetences |> List.isEmpty |> not then
         let addCompetences (competences: Competence seq) (insertAfter: Paragraph) =
             if competences |> Seq.isEmpty then
-                let p = createParagraph "Нет." Stretch false false
+                let p = createParagraph "Нет." Stretch false false true
                 insertAfter.InsertAfterSelf p
             else 
                 competences 
                 |> Seq.fold (fun (lastParagraph: Paragraph) competence -> 
                         let description = $"{competence.Description.[0]}".ToLower() + competence.Description.Substring(1)
-                        let p = createParagraph ($"{competence.Code} — {description}.") Stretch false false
+                        let p = createParagraph ($"{competence.Code} — {description}.") Stretch false false true
                         lastParagraph.InsertAfterSelf p
                     ) insertAfter
 
         let addHeader (header: string) (insertAfter: Paragraph) =
-            createParagraph header Stretch true false
+            createParagraph header Stretch true false true
             |> insertAfter.InsertAfterSelf
 
         let sectionHeader = findParagraph body "Методические материалы для проведения текущего контроля успеваемости и промежуточной аттестации (контрольно-измерительные материалы, оценочные средства)"
@@ -198,7 +317,7 @@ let addCompetencesToAttestationMaterials (content: ProgramContent) (curriculum: 
                 |> addHeader "Компетенции, полностью сформированные по результатам освоения дисциплины:" 
                 |> addCompetences fullyFormedCompetences
         
-            let p = createParagraph "Для каждой компетенции применяется линейная шкала оценивания, определяемая долей успешно выполненных заданий, проверяющих данную компетенцию" Stretch false false
+            let p = createParagraph "Для каждой компетенции применяется линейная шкала оценивания, определяемая долей успешно выполненных заданий, проверяющих данную компетенцию" Stretch false false true
             competencesParagraph.InsertAfterSelf p |> ignore
 
             let sectionHeader = findParagraph body "Методические материалы для оценки обучающимися содержания и качества учебного процесса"
@@ -210,10 +329,10 @@ let addCompetencesToAttestationMaterials (content: ProgramContent) (curriculum: 
                     |> Seq.map (fun c -> c.Code)
                     |> Seq.reduce (fun acc s -> acc + ", " + s)
                 let competences = "Проверяемые компетенции: " + competences
-                let p = createParagraph competences Stretch false true
+                let p = createParagraph competences Stretch false true true
                 sectionHeader.InsertBeforeSelf p |> ignore
 
-                let p = createParagraph "Сформированность компетенций считается пропорционально доле успешных ответов на вопросы и доле выполненных заданий." Stretch false true
+                let p = createParagraph "Сформированность компетенций считается пропорционально доле успешных ответов на вопросы и доле выполненных заданий." Stretch false true true
                 sectionHeader.InsertBeforeSelf p |> ignore
             else
                 printfn "'Методические материалы для оценки обучающимися содержания и качества учебного процесса' не удалось найти, список проверяемых ФОС компетенций не сгенерирован!"
@@ -227,7 +346,7 @@ let fillRequiredLiterature (content: ProgramContent) (body: Body) =
             let sectionHeader = sectionHeader.Value
             printfn "Список обязательной литературы пуст, добавляю 'Не требуется'." 
             removeNextParagraphIfEmpty body sectionHeader true
-            let p = createParagraph "Не требуется" Stretch false false
+            let p = createParagraph "Не требуется" Stretch false false true
             sectionHeader.InsertAfterSelf p |> ignore
         else
             printfn "'Список обязательной литературы' не найден!" 
@@ -239,29 +358,42 @@ let fillOptionalLiterature (content: ProgramContent) (body: Body) =
             let sectionHeader = sectionHeader.Value
             removeNextParagraphIfEmpty body sectionHeader true
             printfn "Список дополнительной литературы пуст, добавляю 'Не требуется'." 
-            let p = createParagraph "Не требуется" Stretch false false
+            let p = createParagraph "Не требуется" Stretch false false true
             sectionHeader.InsertAfterSelf p |> ignore
         else
             printfn "'Список дополнительной литературы' не найден!" 
 
-let addLiterature (body: Body) =
+let addLiterature (document: WordprocessingDocument) (body: Body) =
     let sectionHeader = findParagraph body "Перечень иных информационных источников"
     if sectionHeader.IsSome then
         let sectionHeader = sectionHeader.Value
 
+        let createNumberingPr (numberingId: Int32Value) =
+            let ilvl = NumberingLevelReference(Val = Int32Value(0))
+            let numId = NumberingId(Val = numberingId)
+            let numPr = NumberingProperties(NumberingId = numId, NumberingLevelReference = ilvl)
+            numPr
+
+        let numberingId = createNumbering document
+
         let literature = 
-            [ "Сайт Научной библиотеки им. М. Горького СПбГУ: http://www.library.spbu.ru/"
-              "Электронный каталог Научной библиотеки им. М. Горького СПбГУ: http://www.library.spbu.ru/cgi-bin/irbis64r/cgiirbis_64.exe?C21COM=F&I21DBN=IBIS&P21DBN=IBIS" 
-              "Перечень электронных ресурсов, находящихся в доступе СПбГУ: http://cufts.library.spbu.ru/CRDB/SPBGU/" 
-              "Перечень ЭБС, на платформах которых представлены российские учебники, находящиеся в доступе СПбГУ: http://cufts.library.spbu.ru/CRDB/SPBGU/browse?name=rures&resource_type=8" ]
+            [ "Сайт Научной библиотеки им. М. Горького СПбГУ:", "http://www.library.spbu.ru/"
+              "Электронный каталог Научной библиотеки им. М. Горького СПбГУ:", "http://www.library.spbu.ru/cgi-bin/irbis64r/cgiirbis_64.exe?C21COM=F&I21DBN=IBIS&P21DBN=IBIS" 
+              "Перечень электронных ресурсов, находящихся в доступе СПбГУ:", "http://cufts.library.spbu.ru/CRDB/SPBGU/" 
+              "Перечень ЭБС, на платформах которых представлены российские учебники, находящиеся в доступе СПбГУ:", "http://cufts.library.spbu.ru/CRDB/SPBGU/browse?name=rures&resource_type=8" ]
             |> List.rev
 
-        printfn "Добавляю в перечень иных информационных источников стандартный список литературы (без форматирования!)" 
+        printfn "Добавляю в перечень иных информационных источников стандартный список литературы." 
 
         literature 
-        |> List.iter (fun s ->
-            let p = createParagraph s Stretch false false
-            sectionHeader.InsertAfterSelf p |> ignore)
+        |> List.iter (fun (name, link) ->
+            let nameParagraph = createParagraph name Left false false false
+            let numPr = createNumberingPr numberingId
+            nameParagraph.ParagraphProperties.AppendChild numPr |> ignore
+            sectionHeader.InsertAfterSelf nameParagraph |> ignore
+            let linkParagraph = createHyperlink link document
+            nameParagraph.InsertAfterSelf linkParagraph |> ignore
+            )
     else
         printfn "'Перечень иных информационных источников' не найден, литература не сгенерирована!" 
 
@@ -339,7 +471,7 @@ let patchProgram (curriculumFile: string) (programFileName: string) =
         fillOptionalLiterature content body
 
         if content |> libraryLinksShallPresent |> List.isEmpty |> not then
-            addLiterature body
+            addLiterature wordDocument body
 
         fixStudent content body
 
