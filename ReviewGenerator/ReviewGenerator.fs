33d5f0e0122e43b9a56b57a1c21af65ed3700edd
yurii-litvinov <yurii.litvinov@gmail.com>
2021-12-20 17:34:40 +0300

    Add support of review link and status, some refactoring

--- ReviewGenerator/ReviewGenerator.fs
+++ ReviewGenerator/ReviewGenerator.fs
@@ -17,6 +17,8 @@ type Work =
         p2Score: string
         p2Comment: string
         general: string
+        reviewLink: string
+        status: string
     }
 
 [<EntryPoint>]
@@ -28,7 +30,7 @@ let main argv =
     let page = "Form Responses 1"
     use service = openGoogleSheet "AssignmentMatcher" 
 
-    let sheet = readGoogleSheet service spreadsheetId page "A" "P" offset
+    let sheet = readGoogleSheet service spreadsheetId page "A" "R" offset
 
     let works = sheet 
                 |> Seq.map(fun line -> 
@@ -48,6 +50,8 @@ let main argv =
                         p2Score = line.[13]
                         p2Comment = if line.Length > 14 then line.[14] else ""
                         general = if line.Length > 15 then line.[15] else ""
+                        reviewLink = if line.Length > 16 then line.[16] else ""
+                        status = if line.Length > 17 then line.[17] else ""
                     }
                     )
 
@@ -55,49 +59,41 @@ let main argv =
     |> Seq.iter(fun work ->
         use file = new FileStream(work.name.Replace('\"', ' ').Replace('/', '-').Replace('\\', '-') + ".txt", FileMode.Create)
         use writer = new StreamWriter(file)
+
+        let writeSection (title: string) (score: string) (comment: string) =
+            writer.WriteLine(title)
+            writer.WriteLine(score)
+            if comment <> "" then
+                writer.WriteLine(comment)
+            writer.WriteLine()
+
         writer.WriteLine("Здравствуйте.")
         writer.WriteLine()
-        writer.WriteLine("Вот рецензия, по критериям. Каждый критерий оценивается по пятибалльной шкале.")
+        writer.WriteLine("Вот рецензия на текст практики. Каждый критерий оценивается по пятибалльной шкале.")
         writer.WriteLine()
 
-        writer.WriteLine("О1. Соответствие содержания и оформления предъявленным требованиям:")
-        writer.WriteLine(work.o1Score)
-        writer.WriteLine(work.o1Comment)
-        writer.WriteLine()
+        writeSection "О1. Соответствие содержания и оформления предъявленным требованиям:" work.o1Score work.o1Comment
+        writeSection "О2. Умение работать с информацией, опубликованной в научных и иных источниках:" work.o2Score work.o2Comment
 
-        writer.WriteLine("О2. Умение работать с информацией, опубликованной в научных и иных источниках:")
-        writer.WriteLine(work.o2Score)
-        writer.WriteLine(work.o2Comment)
-        writer.WriteLine()
+        writeSection "Т1. Обоснование принятых решений/Теоретический анализ:" work.t1Score work.t1Comment
+        writeSection "Т2. Сравнение с аналогами:" work.t2Score work.t2Comment
 
-        writer.WriteLine("Т1. Обоснование принятых решений/Теоретический анализ:")
-        writer.WriteLine(work.t1Score)
-        writer.WriteLine(work.t1Comment)
-        writer.WriteLine()
-
-        writer.WriteLine("Т2. Сравнение с аналогами:")
-        writer.WriteLine(work.t2Score)
-        writer.WriteLine(work.t2Comment)
-        writer.WriteLine()
-
-        writer.WriteLine("П1. Качество практической части:")
-        writer.WriteLine(work.p1Score)
-        if work.p1Comment <> "" then
-            writer.WriteLine(work.p1Comment)
-        writer.WriteLine()
-
-        writer.WriteLine("П2. Качество проводимых измерений и постановки экспериментов:")
-        writer.WriteLine(work.p2Score)
-        if work.p2Comment <> "" then
-            writer.WriteLine(work.p2Comment)
-        writer.WriteLine()
+        writeSection "П1. Качество практической части:" work.p1Score work.p1Comment
+        writeSection "П2. Качество проводимых измерений и постановки экспериментов:" work.p2Score work.p2Comment
 
         if work.general <> "" then
             writer.WriteLine("Общий комментарий к работе:")
             writer.WriteLine(work.general)
             writer.WriteLine()
 
-        writer.WriteLine("В общем, надо поправить, выложить в папку и сообщить мне, что работа готова к повторному рецензированию")
+        if work.reviewLink <> "" then
+            writer.WriteLine("Ссылка на подробное ревью: " + work.reviewLink)
+            writer.WriteLine()
+
+        if work.status <> "Можно зачесть" then
+            writer.WriteLine("В общем, надо поправить, выложить в папку и сообщить мне, что работа готова к повторному рецензированию.")
+        else
+            writer.WriteLine("В общем, текст зачтён.")
 
         ()
         )
