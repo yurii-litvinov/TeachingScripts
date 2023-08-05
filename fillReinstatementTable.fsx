15875c508cffe00845032f66498814c543cb5518
yurii-litvinov <yurii.litvinov@gmail.com>
2023-08-05 12:19:22 +0300

    feat: move reinstatement script to Yandex infrastructure

--- fillReinstatementTable.fsx
+++ fillReinstatementTable.fsx
@@ -1,36 +1,42 @@
-let spreadsheetId = "1nyOe9VRPyAGdthXdJKmLBWFhYBu2VycEI-4gv9MjdHo"
+let yandexSheetFolderUrl =
+    @"https://disk.yandex.ru/client/disk/Административная%20деятельность%20СПбГУ/Комиссия%20по%20переводам%20и%20восстановлениям/2023%2C%20лето"
+
+let yandexSheetFileName = @"Оценки"
+
 let dataFileName = "pretendents.xlsx"
 let tabName = "Список полный"
 
 #r "nuget: Google.Apis.Sheets.v4"
 #r "nuget: DocumentFormat.OpenXml"
 
-#load "ReviewGenerator/LessPainfulGoogleSheets.fs"
-#load "AdcEditor/LessPainfulXlsx.fs"
+#load "DocUtils/DocUtils/XlsxUtils.fs"
+#load "DocUtils/DocUtils/YandexSheetsUtils.fs"
 
-open LessPainfulGoogleSheets
-open LessPainfulXlsx
+open DocUtils.YandexSheets
+open DocUtils.Xlsx
+open System
 
 type Statement =
     { Programme: string
-      Course: string 
-      StatementType: string
-      }
+      Course: string
+      StatementType: string }
 
 type Student =
     { Name: string
-      Statements: Statement list
-      }
+      Statements: Statement list }
 
-let collectData () = 
-    let sheet = openXlsxSheet dataFileName tabName
-    
-    let readAndClean columnIndex =
-        readColumn sheet columnIndex |> Seq.skip 2 |> Seq.filter ((<>) "")
+let collectData () =
+    let sheet = Spreadsheet.FromFile(dataFileName).Sheet(tabName)
+
+    let readAndClean (columnIndex: int) =
+        sheet.Column columnIndex |> Seq.skip 2 |> Seq.filter ((<>) "")
 
     let formStatements (statements: (string * string * string * string) seq) =
-        statements 
-        |> Seq.map (fun (_, p, c, st) -> { Programme = p; Course = c; StatementType = st.ToLower () }) 
+        statements
+        |> Seq.map (fun (_, p, c, st) ->
+            { Programme = p
+              Course = c
+              StatementType = st.ToLower() })
         |> Seq.toList
 
     let names = readAndClean 1
@@ -38,18 +44,22 @@ let collectData () =
     let courses = readAndClean 4
     let statementTypes = readAndClean 5
 
-    let merged = 
-        Seq.zip3 names programmes courses 
+    let merged =
+        Seq.zip3 names programmes courses
         |> Seq.zip statementTypes
         |> Seq.map (fun (st, (n, p, c)) -> (n.Trim(), p.Replace('\n', ' ').Trim(), c, st))
 
     let combined = merged |> Seq.groupBy (fun (n, _, _, _) -> n)
-    let result = combined |> Seq.map (fun (name, statements) -> { Name = name; Statements = formStatements statements})
+
+    let result =
+        combined
+        |> Seq.map (fun (name, statements) ->
+            { Name = name
+              Statements = formStatements statements })
+
     result
 
 let data = collectData ()
-
-let service = openGoogleSheet "TeachingScripts" 
 
 let namesColumn = data |> Seq.map (fun student -> student.Name)
 
@@ -58,17 +68,27 @@ let smartFold selector lineBreak =
         if data |> Seq.distinct |> Seq.length = 1 then
             data |> Seq.head
         else
-            data |> Seq.reduce (fun acc el -> $"{acc},{if lineBreak then '\n' else ' '}{el}")
+            data
+            |> Seq.reduce (fun acc el -> $"{acc},{if lineBreak then '\n' else ' '}{el}")
 
-    data |> Seq.map (fun student -> student.Statements |> Seq.map selector |> smartReduce)
+    data
+    |> Seq.map (fun student -> student.Statements |> Seq.map selector |> smartReduce)
 
 let programmesColumn = smartFold (fun st -> st.Programme) true
 let coursesColumn = smartFold (fun st -> st.Course) false
 let statementTypeColumn = smartFold (fun st -> st.StatementType) true
 
-writeGoogleSheetColumn service spreadsheetId "Сортированные по алфавиту" "A" 2 namesColumn
-writeGoogleSheetColumn service spreadsheetId "Сортированные по алфавиту" "C" 2 programmesColumn
-writeGoogleSheetColumn service spreadsheetId "Сортированные по алфавиту" "D" 2 coursesColumn
-writeGoogleSheetColumn service spreadsheetId "Сортированные по алфавиту" "E" 2 statementTypeColumn
+let service = YandexService.FromClientSecretsFile()
 
-service.Dispose()
+let yandexSpreadsheet =
+    service
+        .GetSpreadsheetByFolderAndFileNameAsync(yandexSheetFolderUrl, yandexSheetFileName)
+        .Result
+
+let sheet = yandexSpreadsheet.Sheet "Сортированные по алфавиту"
+sheet.WriteColumn 0 2 namesColumn
+sheet.WriteColumn 2 2 programmesColumn
+sheet.WriteColumn 3 2 coursesColumn
+sheet.WriteColumn 4 2 statementTypeColumn
+
+yandexSpreadsheet.SaveAsync() |> Async.AwaitTask |> Async.RunSynchronously
