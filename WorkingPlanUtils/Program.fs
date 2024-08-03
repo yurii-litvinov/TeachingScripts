81d10e3797b7c80de4ed707c5dc5dbf27ea86547
yurii-litvinov <yurii.litvinov@gmail.com>
2024-08-03 17:25:48 +0300

    feat: add calculating differences in working plans up to a specified semester, add Argu for arguments parsing

--- WorkingPlanUtils/Program.fs
+++ WorkingPlanUtils/Program.fs
@@ -1,114 +1,41 @@
-open CurriculumParser
+open UseCases
 open System.IO
+open Argu
 
-let plansFolder = "../WorkingPlans"
-
-let planNameToCode fileName =
-    FileInfo(fileName).Name.Substring(3, "9999-2084".Length)
-
-let planCodeToFileName planCode =
-    Directory.EnumerateFiles (System.AppDomain.CurrentDomain.BaseDirectory + "/../../../" + plansFolder)
-    |> Seq.find (fun f -> planNameToCode f = planCode)
-
-let comparePlans oldPlan newPlan =
-    let oldCurriculum = DocxCurriculum(planCodeToFileName oldPlan)
-    let newCurriculum = DocxCurriculum(planCodeToFileName newPlan)
-
-    let oldPlanDisciplines = oldCurriculum.Disciplines 
-    let newPlanDisciplines = newCurriculum.Disciplines 
-
-    let sharedDisciplines = oldPlanDisciplines |> Seq.filter (fun d -> newPlanDisciplines |> Seq.exists (fun nd -> d.Code = nd.Code))
-
-    let str (discipline: Discipline) = sprintf "[%s] '%s'" discipline.Code discipline.RussianName
-
-    printfn "Дисциплины плана %s, которых нет в %s:" oldPlan newPlan
-    oldPlanDisciplines |> Seq.filter (fun d -> sharedDisciplines |> Seq.contains d |> not) |> Seq.iter (printfn "%s" << str)
-    printfn ""
-
-    printfn "Дисциплины плана %s, которых нет в %s:" newPlan oldPlan
-    newPlanDisciplines |> Seq.filter (fun d -> sharedDisciplines |> Seq.exists (fun sd -> sd.Code = d.Code) |> not) |> Seq.iter (printfn "%s" << str)
-    printfn ""
-
-    let matchingPairs = sharedDisciplines |> Seq.map (fun d -> (d, newPlanDisciplines |> Seq.find (fun nd -> d.Code = nd.Code)))
-
-    let laborIntensity (discipline: Discipline) = discipline.Implementations |> Seq.sumBy (fun i -> i.LaborIntensity)
-    let attestationTypes (discipline: Discipline) = discipline.Implementations |> Seq.map (fun i -> i.MonitoringTypes) |> Seq.toList
-    let semesters (discipline: Discipline) = discipline.Implementations |> Seq.map (fun i -> i.Semester) |> Seq.toList
-    let hours (discipline: Discipline) = discipline.Implementations |> Seq.map (fun i -> i.WorkHours) |> Seq.toList
-
-    printfn ""
-    printfn "Разница в трудоёмкости:"
-    matchingPairs 
-    |> Seq.filter (fun (o, n) -> (laborIntensity o) <> (laborIntensity n))
-    |> Seq.iter (fun (o, n) -> printfn "%s: трудоёмкость в %s: %d, в %s: %d" (str o) oldPlan (laborIntensity o) newPlan (laborIntensity n))
-
-    printfn ""
-    printfn "Разница в семестрах реализации:"
-    matchingPairs 
-    |> Seq.filter (fun (o, n) -> (semesters o) <> (semesters n))
-    |> Seq.iter (fun (o, n) -> printfn "%s: семестры в %s: %A, в %s: %A" (str o) oldPlan (semesters o) newPlan (semesters n))
-
-    printfn ""
-    printfn "Разница в форме аттестации:"
-    matchingPairs 
-    |> Seq.filter (fun (o, n) -> (attestationTypes o) <> (attestationTypes n))
-    |> Seq.iter (fun (o, n) -> printfn "%s: формы аттестации в %s: %A, в %s: %A" (str o) oldPlan (attestationTypes o) newPlan (attestationTypes n))
-
-    printfn ""
-    printfn "Разница в часах:"
-    matchingPairs 
-    |> Seq.filter (fun (o, n) -> (hours o) <> (hours n))
-    |> Seq.iter (fun (o, n) -> printfn "%s: часы в %s: %A, в %s: %A" (str o) oldPlan (hours o) newPlan (hours n))
-
-    printfn ""
-    let oldPlanCompetences = oldCurriculum.Competences
-    let newPlanCompetences = newCurriculum.Competences
-    let sharedCompetences = oldPlanCompetences |> Seq.filter (fun c -> newPlanCompetences |> Seq.exists (fun nc -> c.Code = nc.Code && c.Description = nc.Description))
-
-    printfn "Компетенции плана %s, которых нет в %s:" oldPlan newPlan
-    oldPlanCompetences |> Seq.filter (fun с -> sharedCompetences |> Seq.contains с |> not) |> Seq.iter (fun c -> printfn "%s %s" c.Code c.Description)
-    printfn ""
-
-    printfn "Компетенции плана %s, которых нет в %s:" newPlan oldPlan
-    newPlanCompetences |> Seq.filter (fun c -> sharedCompetences |> Seq.exists (fun oc -> oc.Code = c.Code && oc.Description = c.Description) |> not) |> Seq.iter (fun c -> printfn "%s %s" c.Code c.Description)
-    printfn ""
-
-    ()
-
-let printBySemester plan semester =
-    let curriculum = DocxCurriculum(planCodeToFileName plan)
-    curriculum.Disciplines
-    |> Seq.filter (fun d -> d.Implementations |> Seq.exists (fun i -> i.Semester = int semester))
-    |> Seq.sortBy (fun d -> d.Code)
-    |> Seq.iter (fun d -> printfn "[%s] %s" d.Code d.RussianName)
-
-
-let printHelp () =
-    printfn "Добро пожаловать в Working Plan Utilities, набор инструментов для работы с учебными планами."
-    printfn "Использование:"
-    printfn "\tdotnet run -- -p <номер рабочего плана и год> <номер семестра>"
-    printfn "\t\t- распечатать отсортированный список дисциплин в данном семестре."
-    printfn "\tdotnet run -- -c <номер рабочего плана и год 1> <номер рабочего плана и год 2>"
-    printfn "\t\t- сравнить два учебных плана."
-    printfn ""
-    printfn "Имеющиеся учебные планы:"
-    Directory.EnumerateFiles(plansFolder) 
-    |> Seq.map (fun p -> FileInfo(p).Name.Substring(3, "9999-2084".Length))
-    |> Seq.iter (printf "%s ")
+type Arguments = 
+    | [<CliPrefix(CliPrefix.None); Unique; First>] Print_By_Semester of plan: string * semester: int
+    | [<CliPrefix(CliPrefix.None); Unique; First>] Compare of plan1: string * plan2: string
+    | [<CliPrefix(CliPrefix.None); Unique; First>] Compare_To_Semester of plan1: string * plan2: string * semester: int
+    interface IArgParserTemplate with
+        member this.Usage: string = 
+            match this with
+            | Print_By_Semester _ -> "specify a plan code and a semester."
+            | Compare _ -> "specify two working plans"
+            | Compare_To_Semester _ -> "specify two working plans and a semester up to which they shall be compared"
 
 [<EntryPoint>]
 let main argv =
-    if argv.Length < 2 then
-        printHelp ()
-    else
-        let call f =
-            if argv.Length < 3 then
-                printHelp ()
-            else
-                f argv.[1] argv.[2]
+    let parser = ArgumentParser.Create<Arguments>(programName = "WorkingPlanUtils.exe")
+    
+    let result = parser.Parse (inputs=argv, raiseOnUsage=false)
 
-        match argv.[0] with
-        | "--print-by-semester" | "-p" -> call printBySemester
-        | "--compare" | "-c" -> call comparePlans
-        | _ -> printHelp ()
+    if result.Contains Print_By_Semester then
+        let plan, semester = result.GetResult Print_By_Semester
+        printBySemester plan semester
+    elif result.Contains Compare then
+        let plan1, plan2 = result.GetResult Compare
+        comparePlans plan1 plan2
+    elif result.Contains Compare_To_Semester then
+        let plan1, plan2, semester = result.GetResult Compare_To_Semester
+        compareToSemester plan1 plan2 semester
+    else
+        printfn "%s" <| parser.PrintUsage()
+
+        let existingPlans =
+            Directory.EnumerateFiles(plansFolder) 
+            |> Seq.map (fun p -> FileInfo(p).Name.Substring(3, "9999-2084".Length) + " ")
+            |> Seq.reduce (+)
+
+        printfn "\n\nExisting plans:\n %s" existingPlans
+
     0
---
+++ WorkingPlanUtils/UseCases.fs
@@ -0,0 +1,102 @@
+﻿module UseCases
+
+open System.IO
+open CurriculumParser
+
+let plansFolder = "../WorkingPlans"
+
+let planNameToCode fileName =
+    FileInfo(fileName).Name.Substring(3, "9999-2084".Length)
+
+let planCodeToFileName planCode =
+    Directory.EnumerateFiles (System.AppDomain.CurrentDomain.BaseDirectory + "/../../../" + plansFolder)
+    |> Seq.find (fun f -> planNameToCode f = planCode)
+
+let compareToSemester oldPlan newPlan semester =
+    let oldCurriculum = DocxCurriculum(planCodeToFileName oldPlan)
+    let newCurriculum = DocxCurriculum(planCodeToFileName newPlan)
+
+    let filterBySemester (disciplines: Discipline seq) =
+        disciplines |> Seq.filter (fun d -> d.Implementations |> Seq.exists(fun i -> i.Semester <= semester))
+
+    let oldPlanDisciplines = oldCurriculum.Disciplines |> filterBySemester
+    let newPlanDisciplines = newCurriculum.Disciplines |> filterBySemester
+
+    let sharedDisciplines = oldPlanDisciplines |> Seq.filter (fun d -> newPlanDisciplines |> Seq.exists (fun nd -> d.Code = nd.Code))
+
+    let str (discipline: Discipline) = sprintf "[%s] '%s'" discipline.Code discipline.RussianName
+
+    if semester < 8 then
+        printfn "Сравнение учебных планов %s и %s до семестра %i. \n\n" oldPlan newPlan semester
+
+    printfn "Дисциплины плана %s, которых нет в %s:" oldPlan newPlan
+    oldPlanDisciplines |> Seq.filter (fun d -> sharedDisciplines |> Seq.contains d |> not) |> Seq.iter (printfn "%s" << str)
+    printfn ""
+
+    printfn "Дисциплины плана %s, которых нет в %s:" newPlan oldPlan
+    newPlanDisciplines |> Seq.filter (fun d -> sharedDisciplines |> Seq.exists (fun sd -> sd.Code = d.Code) |> not) |> Seq.iter (printfn "%s" << str)
+    printfn ""
+
+    let matchingPairs = sharedDisciplines |> Seq.map (fun d -> (d, newPlanDisciplines |> Seq.find (fun nd -> d.Code = nd.Code)))
+
+    let laborIntensity (discipline: Discipline) = discipline.Implementations |> Seq.sumBy (fun i -> i.LaborIntensity)
+    let attestationTypes (discipline: Discipline) = discipline.Implementations |> Seq.map (fun i -> i.MonitoringTypes) |> Seq.toList
+    let semesters (discipline: Discipline) = discipline.Implementations |> Seq.map (fun i -> i.Semester) |> Seq.toList
+    let hours (discipline: Discipline) = discipline.Implementations |> Seq.map (fun i -> i.WorkHours) |> Seq.toList
+
+    printfn ""
+    printfn "Разница в трудоёмкости:"
+    matchingPairs 
+    |> Seq.filter (fun (o, n) -> (laborIntensity o) <> (laborIntensity n))
+    |> Seq.iter (fun (o, n) -> printfn "%s: трудоёмкость в %s: %d, в %s: %d" (str o) oldPlan (laborIntensity o) newPlan (laborIntensity n))
+
+    printfn ""
+    printfn "Разница в семестрах реализации:"
+    matchingPairs 
+    |> Seq.filter (fun (o, n) -> (semesters o) <> (semesters n))
+    |> Seq.iter (fun (o, n) -> printfn "%s: семестры в %s: %A, в %s: %A" (str o) oldPlan (semesters o) newPlan (semesters n))
+
+    printfn ""
+    printfn "Разница в форме аттестации:"
+    matchingPairs 
+    |> Seq.filter (fun (o, n) -> (attestationTypes o) <> (attestationTypes n))
+    |> Seq.iter (fun (o, n) -> printfn "%s: формы аттестации в %s: %A, в %s: %A" (str o) oldPlan (attestationTypes o) newPlan (attestationTypes n))
+
+    printfn ""
+    printfn "Разница в часах:"
+    matchingPairs 
+    |> Seq.filter (fun (o, n) -> (hours o) <> (hours n))
+    |> Seq.iter (fun (o, n) -> printfn "%s: часы в %s: %A, в %s: %A" (str o) oldPlan (hours o) newPlan (hours n))
+
+    printfn ""
+    let oldPlanCompetences = oldCurriculum.Competences
+    let newPlanCompetences = newCurriculum.Competences
+    let sharedCompetences = oldPlanCompetences |> Seq.filter (fun c -> newPlanCompetences |> Seq.exists (fun nc -> c.Code = nc.Code && c.Description = nc.Description))
+
+    printfn "Компетенции плана %s, которых нет в %s:" oldPlan newPlan
+    oldPlanCompetences |> Seq.filter (fun с -> sharedCompetences |> Seq.contains с |> not) |> Seq.iter (fun c -> printfn "%s %s" c.Code c.Description)
+    printfn ""
+
+    printfn "Компетенции плана %s, которых нет в %s:" newPlan oldPlan
+    newPlanCompetences |> Seq.filter (fun c -> sharedCompetences |> Seq.exists (fun oc -> oc.Code = c.Code && oc.Description = c.Description) |> not) |> Seq.iter (fun c -> printfn "%s %s" c.Code c.Description)
+    printfn ""
+
+    ()
+
+let comparePlans oldPlan newPlan =
+    compareToSemester oldPlan newPlan 8
+
+let printBySemester plan semester =
+    let printDiscipline (discipline: Discipline) =
+        let implementation = discipline.Implementations |> Seq.find (fun i -> i.Semester = int semester)
+        let workHours = implementation.WorkHours.Split(' ')
+        let lectures = int workHours[0]
+        let practices = [1; 3; 4; 5] |> List.map (fun i -> int workHours[i]) |> List.sum
+        let homework = int workHours[9] + int workHours[11]
+        printfn "[%s] %s\t%i\t%i\t%i" discipline.Code discipline.RussianName lectures practices homework
+
+    let curriculum = DocxCurriculum(planCodeToFileName plan)
+    curriculum.Disciplines
+    |> Seq.filter (fun d -> d.Implementations |> Seq.exists (fun i -> i.Semester = int semester))
+    |> Seq.sortBy (fun d -> d.Code)
+    |> Seq.iter (fun d -> printDiscipline d)
